aaca527c17b60c3735435441e2034d12b7062469
Shubhang Agrawal <shubhangagrawal@Shubhangs-MacBook-Air.local>
2025-10-08 16:38:18 +0530

    Refactor

--- src/main/java/com/example/demo/service/OrderService.java
+++ src/main/java/com/example/demo/service/OrderService.java
@@ -2,7 +2,6 @@ package com.example.demo.service;
 
 import com.example.demo.exception.OrderNotFoundException;
 import com.example.demo.exception.PartnerNotFoundException;
-import com.example.demo.model.dao.Location;
 import com.example.demo.model.dao.Order;
 import com.example.demo.model.dao.Partner;
 import com.example.demo.model.enums.PartnerStatus;
@@ -21,7 +20,7 @@ public class OrderService {
     }
 
     public Order assignOrder(Order order) {
-        Partner nearestPartner = findNearestAvailablePartner(order.getPickupLocation());
+        Partner nearestPartner = partnerService.findNearestAvailablePartner(order.getPickupLocation());
         if (nearestPartner != null) {
             order.setPartnerId(nearestPartner.getId());
             partnerService.updatePartnerStatus(nearestPartner.getId(), PartnerStatus.BUSY);
@@ -38,17 +37,5 @@ public class OrderService {
             return order;
         }
         throw new OrderNotFoundException("Invalid OrderId!");
-    }
-
-    private Partner findNearestAvailablePartner(Location pickupLocation) {
-        return partnerService.getAllPartners().stream()
-                .filter(p -> p.getStatus() == PartnerStatus.AVAILABLE)
-                .min((p1, p2) -> Double.compare(calculateDistance(p1.getLocation(), pickupLocation),
-                        calculateDistance(p2.getLocation(), pickupLocation)))
-                .orElse(null);
-    }
-
-    private double calculateDistance(Location loc1, Location loc2) {
-                return Math.abs(loc1.getX() - loc2.getX()) + Math.abs(loc1.getY() - loc2.getY());
     }
 }
--- src/main/java/com/example/demo/service/PartnerService.java
+++ src/main/java/com/example/demo/service/PartnerService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.exception.PartnerNotFoundException;
+import com.example.demo.model.dao.Location;
 import com.example.demo.model.dao.Partner;
 import com.example.demo.model.enums.PartnerStatus;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,18 @@ public class PartnerService {
             return partner;
         }
         throw new PartnerNotFoundException("Partner not found!");
+    }
+
+    public Partner findNearestAvailablePartner(Location pickupLocation) {
+        return getAllPartners().stream()
+                .filter(p -> p.getStatus() == PartnerStatus.AVAILABLE)
+                .min((p1, p2) -> Double.compare(calculateDistance(p1.getLocation(), pickupLocation),
+                        calculateDistance(p2.getLocation(), pickupLocation)))
+                .orElse(null);
+    }
+
+    private double calculateDistance(Location loc1, Location loc2) {
+        return Math.abs(loc1.getX() - loc2.getX()) + Math.abs(loc1.getY() - loc2.getY());
     }
 
     public Collection<Partner> getAllPartners() {
